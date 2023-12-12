c125ce61a69b03b533469d1c21c65c03c76d58b9
JangWheesung <gameplay1qaz@gmail.com>
2023-12-13 00:51:36 +0900

    업그레이드매니저 생성

---
+++ 2023_winapi_framework/LevelMgr.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "TimeMgr.h"
+#include "Scene.h"
+#include "SceneMgr.h"
+#include "LevelMgr.h"
+
+void LevelMgr::UpgradePanelUp()
+{
+
+}
+
+void LevelMgr::UpgradePanelDown()
+{
+
+}
+
+void LevelMgr::OnUpgradeBtnClick()
+{
+
+}
+
+void LevelMgr::RandomWeapon()
+{
+
+}
+
+void LevelMgr::UpgradeWeapon()
+{
+
+}
